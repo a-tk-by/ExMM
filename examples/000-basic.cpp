89989c49ac1e9686ca44ce6d9661a7ed18a1fd93
Alexey Tkachenko <alex_tk@tut.by>
2019-07-18 16:11:36 +0800

    Minimal demo. Not working until no platform supports implemented

--- examples/000-basic.cpp
+++ examples/000-basic.cpp
@@ -1,8 +1,28 @@
 #include "../utilities/examples-registry.hpp"
 #include <iostream>
 
+#include "../src/exmm.hpp"
+
+using namespace ExMM;
+
+volatile struct Registers
+{
+    volatile int A;
+    volatile int B;
+    volatile int C;
+};
+
+class Controller : public ControllerBase<HookTypes::None, Registers>
+{
+};
+
 EXMM_DEMO(Basic)
 {
     output << "Basic demo stub" << std::endl;
-    return true;
+
+    const Controller controller;
+    auto *registers = controller.GetIoSpace();
+    
+    registers->A = 42;
+    return registers->A == 42;
 }
