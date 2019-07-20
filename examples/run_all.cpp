eff95bddf736c0f83af430f75fe2874f51207833
Alexey Tkachenko <alex_tk@tut.by>
2019-07-20 09:34:41 +0800

    Argument types of lambda specified explicitely

--- examples/run_all.cpp
+++ examples/run_all.cpp
@@ -6,8 +6,8 @@ int main(int, char**)
     return
     ExamplesRegistry::RunAll(
         std::cout,
-        [](auto& callback) {std::cout << "[" << callback.Name << "]" << std::endl; },
-        [](auto& callback) {std::cout << "<complete>" << std::endl << std::endl; },
-        [](auto& callback) {std::cerr << "<failed>" << std::endl << std::endl;}
+        [](const ExamplesRegistry::Callback& callback) {std::cout << "[" << callback.Name << "]" << std::endl; },
+        [](const ExamplesRegistry::Callback& callback) {std::cout << "<complete>" << std::endl << std::endl; },
+        [](const ExamplesRegistry::Callback& callback) {std::cerr << "<failed>" << std::endl << std::endl;}
         ) ? 0 : 1;
 }
