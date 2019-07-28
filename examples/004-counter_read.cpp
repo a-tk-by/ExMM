3e87d81aa2ee433381a12ea5d1eca3a49d207f85
Alexey Tkachenko <alex_tk@tut.by>
2019-07-28 21:55:16 +0300

    New model: read access counter

---
+++ examples/004-counter_read.cpp
@@ -0,0 +1,60 @@
+#include "../utilities/examples-registry.hpp"
+
+#include "../src/exmm.hpp"
+
+#include <iostream>
+#include <set>
+#include <cstddef>
+#include <vector>
+
+using namespace ExMM;
+
+struct Registers
+{
+    volatile int A;
+    volatile int B;
+    volatile int C;
+};
+
+struct Controller004 final : public ControllerBase<HookTypes::Read, Registers>
+{
+    void HookRead(Registers* data, size_t offset) override
+    {
+        std::cout << "Before read at offset " << std::hex << offset << std::endl;
+        if (offset == 0)
+        {
+            data->A++;
+            std::cout << "Register A was incremented" << std::endl;
+        }
+    }
+};
+
+EXMM_DEMO(ReadAccessCounter)
+{
+    output << "Register A counts read access" << std::endl;
+
+    Controller004 controller;
+    auto *registers = controller.GetIoSpace();
+
+    std::vector<int> values;
+
+    ExMM::Run([&values, &registers]()
+    {
+        int x = registers->A;
+        values.push_back(x);
+        x = registers->A;
+        values.push_back(x);
+        x = registers->A;
+        values.push_back(x);
+
+        x = registers->C;
+        values.push_back(x);
+    });
+
+    return values.size() == 4
+        && values[0] == 1
+        && values[1] == 2
+        && values[2] == 3
+        && values[3] == 0
+        ;
+}
