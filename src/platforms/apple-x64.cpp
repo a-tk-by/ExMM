afc4a1255ec02ed640d937296d49b7850401d9f5
Svetlana Bayarovich <svetlana.bayarovich@mapbox.com>
2019-11-09 20:57:56 +0300

    Apple support

--- src/platforms/apple-x64.cpp
+++ src/platforms/apple-x64.cpp
@@ -1,4 +1,4 @@
-#if !defined(_WIN32) && defined(__x86_64__) && !defined(__APPLE__)
+#if defined(__x86_64__) && defined(__APPLE__)
 
 #include <csignal>
 
@@ -9,32 +9,33 @@
 bool ExMM::Posix::IsMemoryWriteAccess(void* _context)
 {
     auto context = reinterpret_cast<ucontext_t*>(_context);
-    return context->uc_mcontext.gregs[REG_ERR] & 2;
+    auto& mcontext = *context->uc_mcontext;
+    return mcontext.__es.__err & 2;
 }
 
 void* ExMM::Posix::GetInstructionAddress(void* _context)
 {
     auto context = reinterpret_cast<ucontext_t*>(_context);
-    return reinterpret_cast<void*>(context->uc_mcontext.gregs[REG_RIP]);
+    return reinterpret_cast<void*>(context->uc_mcontext->__ss.__rip);
 }
 
 void ExMM::Platform::InstallBreakPoint(void* context, void* instruction, IoSpace* ioSpace)
 {
     BreakPointData::Get().Set(ioSpace);
-    reinterpret_cast<ucontext_t*>(context)->uc_mcontext.gregs[REG_EFL] |= 0x100;
+    reinterpret_cast<ucontext_t*>(context)->uc_mcontext->__ss.__rflags |= 0x100;
 }
 
 void ExMM::Platform::InstallBreakPoint(void* context, void* instruction, IoSpace* ioSpace, ControllerInterface* controller,
     size_t offset)
 {
     BreakPointData::Get().Set(ioSpace, controller, offset);
-    reinterpret_cast<ucontext_t*>(context)->uc_mcontext.gregs[REG_EFL] |= 0x100;
+    reinterpret_cast<ucontext_t*>(context)->uc_mcontext->__ss.__rflags |= 0x100;
 }
 
 void ExMM::Platform::UninstallBreakPoint(void* context)
 {
     BreakPointData::Get().Unset();
-    reinterpret_cast<ucontext_t*>(context)->uc_mcontext.gregs[REG_EFL] &=~ 0x100;
+    reinterpret_cast<ucontext_t*>(context)->uc_mcontext->__ss.__rflags &= ~0x100;
 }
 
 
