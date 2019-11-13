149b69d24f03c7623499d188c48427b5b5fe178c
Павел Арабей <aps7@tut.by>
2019-11-13 10:07:09 +0300

    remove lcov remove apt quiet update

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,11 +1,11 @@
 
-
-sudo apt-get -qq update
-sudo apt install cmake gcov-8 lcov
+lsb_release -a
+sudo apt-get update
+sudo apt install cmake  lcov
 
 if [ "$TARGET_CPU" == "x86" ]; then
     sudo dpkg --add-architecture i386
-    sudo apt-get -qq update
+    sudo apt-get update
 
     # g++-multilib ставим в самом конце, после i386-пакетов!
     sudo apt-get install -y g++-8-multilib
