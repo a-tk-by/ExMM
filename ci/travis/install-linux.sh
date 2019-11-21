9b3f0a3389477b1619959968632e21e30acab598
J-Roux <aps7@tut.by>
2019-11-21 23:09:03 +0300

    add package for clang x86

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -7,8 +7,6 @@ if [ "$TARGET_CPU" == "x86" ]; then
     sudo dpkg --add-architecture i386
     sudo apt-get update
     # g++-multilib ставим в самом конце, после i386-пакетов!
-    sudo apt-get install -y g++-multilib gcc-multilib
-
-
+    sudo apt-get install -y g++-multilib gcc-multilib linux-libc-dev:i386
 
 fi
